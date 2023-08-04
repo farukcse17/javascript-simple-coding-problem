6c0476bd362bbeeecf4ac53abc5b87129c2eb9dd
Md.Faruq uzzaman <farukcse17.faruk.jsor@gmail.com>
2023-08-04 23:04:40 +0600

    first commit

---
+++ problem1.js
@@ -0,0 +1,18 @@
+// let numbers = [3, 5, 2, 6, 7, 29, 20, 1];
+// let minNumber = Math.max(3, 5, 2, 6, 7, 29, 20, 1);
+// console.log(minNumber);
+
+// Qst: write a code. find out min num from an array
+
+function minNumber(numbers){
+    const minNumbers = numbers[0];
+    for(let i = 0; i < numbers.length; i++){
+        let element = numbers[i];
+        console.log(element);
+    }
+    // return minNumbers;
+}
+
+const numbers = [4, 5, 6, 2, 3, 9, 23, 48, 50, 22];
+const result = minNumber(numbers);
+console.log(result);
